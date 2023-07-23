075a3d0fb1d1c307726119f177afd03ecd791705
Amirhosein Kakavand <94612720+kaakavand@users.noreply.github.com>
2023-07-23 16:28:39 +0330

    fix

--- routes/category.ts
+++ routes/category.ts
@@ -5,7 +5,7 @@ const router = require('express').Router();
 
 router.get('/category', async (req: any, res: any) => {
     const categoryes = await category.find({})
-    res.json({ data: categoryes });
+    res.json({ items: categoryes.map(el => ({ title: el.title, description: el.description , id : el._id})) });
 });
 
 router.post('/category', async (req: any, res: any) => {
@@ -14,7 +14,7 @@ router.post('/category', async (req: any, res: any) => {
         title: req.body.title,
         description: req.body.description
     })
-    
+
     await newCaategory.save()
     res.end()
 
